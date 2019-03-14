8a73cd478fde3b3c815600a7c5b07b81138ddab3
timmila <tim.mila@7summitsagency.com>
2019-03-13 20:55:57 -0500

    Load more branch locations

--- src/component/BranchList.js
+++ src/component/BranchList.js
@@ -5,33 +5,47 @@ import { Link } from "react-router-dom";
 
 class BranchList extends Component {
 
+    
     constructor(props) {
         super(props)
         this.state = {
+            totalLocations: 0,
+            currentOffset: 0,
             locations: []
         }
+        this.loadMore = this.loadMore.bind(this);
     }
 
-    getData = (institution) => {
+    getData = (institution, offset) => {
         console.log("BranchList::getData::" + window.location.href, institution);
-        axios.get(process.env.REACT_APP_API_URL + "/FdicBranchSearch?institutionName=" + institution + "&fields=NAME,OFFNAME,ADDRESS,CITY,STALP,ZIP", {
+        axios.get(process.env.REACT_APP_API_URL + "/FdicBranchSearch?institutionName=" + institution + "&fields=NAME,OFFNAME,ADDRESS,CITY,STALP,ZIP&offset=" + offset, {
             crossdomain: true,
-        })
-        .then(response => this.setState({"locations": response.data.data}));
+        }).then(response => {
+            let locations = this.state.locations;
+            response.data.data.forEach(element => {
+                locations.push(element);
+            });
+            this.setState({"locations": locations, totalLocations: response.data.meta.total, currentOffset: parseInt(response.data.meta.parameters.offset)});
+        });
     }
 
     componentDidMount() {
         console.log("BranchList::componentDidMount::" + window.location.href, this.props.institution);
-        this.getData(this.props.institution);
+        this.getData(this.props.institution, 0);
     }
 
-    componentWillReceiveProps(props) {
-        console.log("BranchList::componentWillReceiveProps::" + window.location.href, props.institution);
-        this.getData(props.institution);
+    // componentWillReceiveProps(props) {
+    //     console.log("BranchList::componentWillReceiveProps::" + window.location.href, props.institution);
+    //     this.getData(props.institution, 0);
+    // }
+
+    loadMore() {
+        console.log("Load more locations");
+        this.getData(this.props.institution, this.state.currentOffset + 10);
     }
+
 
     render() {
-        console.log("BranchList::render::" + window.location.href, this.props.institution);
         let locations = this.state.locations.map((item) =>
             <Link to={"/branch/" + encodeURIComponent(item.data.OFFNAME)} key={item.data.OFFNAME} className="list-group-item list-group-item-action">
                 <span>
@@ -39,6 +53,8 @@ class BranchList extends Component {
                 </span>
             </Link>
         );
+        let canLoadMore = this.state.totalLocations > (10 + this.state.currentOffset);
+        
         return (
             <div>
                 <div className="container">
@@ -46,6 +62,19 @@ class BranchList extends Component {
                         <div className="list-group">
                             { locations }
                         </div>
+                        {canLoadMore &&
+                            <div className="row mt-2">
+                                <div className="col">
+                                    <hr/>
+                                </div> 
+                                <div className="col-2 text-center">
+                                    <button className="btn btn-light" onClick={this.loadMore}>Load More</button>
+                                </div>
+                                <div className="col">
+                                    <hr/>
+                                </div> 
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
